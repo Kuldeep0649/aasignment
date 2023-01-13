d486b571aef2e7a05ffa3d981dcbcc962b7e9a41
Kuldeep0649 <121673377+Kuldeep0649@users.noreply.github.com>
2023-01-13 09:29:11 +0530

    Add files via upload

---
+++ Programming Assignment_1 (2) (1).py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  Programming Assignment_1 
+
+# 1. Write a Python program to print "Hello Python"?
+#   print('hello python')
+
+#  2. Write a Python program to do arithmetical operations addition and division.?
+#    a=10
+#    b=20
+#    print(a+b)
+#    print(b/a)
+
+# 3. Write a Python program to find the area of a triangle?
+# b=int(input("enter base"))
+# h=int(input("enter height"))
+# area=1/2*b*h
+# print("the area of triangle is:",area)
+# 
+# 
+
+#  4. Write a Python program to swap two variables?
+#  x =5
+#  y=10
+#  x,y =y,x
+#  print("x=",x)
+#  print("y=",y)
+# 
+
+# 5. Write a Python program to generate a random number?
+# import random
+# print(random.randint(1,100))
